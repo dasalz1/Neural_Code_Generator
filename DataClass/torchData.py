17ab8972395018055a2d2387948e3e54d1250d97
Daniel Salz <dasalz@stanford.edu>
2020-02-24 18:42:08 -0800

    parallel tokenization and batch retrieval

--- DataClass/torchData.py
+++ DataClass/torchData.py
@@ -1,14 +1,9 @@
-from torch.utils.data import Dataset, Dataset, ConcatDataset, DataLoader
-import pandas as pd
-import torch, os
-
-
 class PairDatasetLazy(Dataset):
 
     def __init__(self, filename, chunksize):
         
         self.filename = filename
-        self.chunksize = chunksize
+        self.chunksize = 1 # more than this and requires a lot more changes in collate fn
         temp = next(pd.read_csv(self.filename, skiprows = 0, chunksize=1, header=None))
         self.len = int(temp.values[0][0] / self.chunksize)
         
@@ -20,12 +15,23 @@ class PairDatasetLazy(Dataset):
                             skiprows=idx * self.chunksize+1,
                             chunksize=self.chunksize, header=None, dtype=str)).fillna('OSOFo').values
         
-        x_tokens = tokenize_fine_grained(x[0, 0])# for idx in range(self.chunksize)]
-        y_tokens = tokenize_fine_grained(x[0, 1])# for idx in range(self.chunksize)]
+        x_tokens = tokenize_fine_grained(x[0, 0])
+        y_tokens = tokenize_fine_grained(x[0, 1])
 
         return x_tokens, y_tokens
+    
+    
+def batch_collate_fn(data):
+        x, y = zip(*data)
+        # off by one in tokens dictionary
+        x = pd.DataFrame(x).replace(token_dict).fillna(-1)+1
+        y = pd.DataFrame(y).replace(token_dict).fillna(-1)+1
+        batch_xs = torch.LongTensor(x.values)
+        batch_ys = torch.LongTensor(y.values)
+        return batch_xs, batch_ys
+    
+    
 
-# dataset dir is directory assuming new subdirectories
 def createDataLoaderAllFiles(dataset_dir, dataset_class=PairDatasetLazy, shuffle=True, batch_size=128):
     datasets = list(filter(lambda x: True if x.endswith('.csv') else False, next(os.walk(dataset_dir))[2]))
-    return DataLoader(ConcatDataset([dataset_class(dataset_dir + '/' + dataset, chunksize=batch_size) for dataset in datasets]), batch_size=batch_size, shuffle=shuffle)
+    return DataLoader(ConcatDataset([dataset_class(dataset_dir + '/' + dataset, chunksize=batch_size) for dataset in datasets]), batch_size=batch_size, shuffle=shuffle, collate_fn=batch_collate_fn)
