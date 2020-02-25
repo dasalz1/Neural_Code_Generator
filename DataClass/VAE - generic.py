fce10f7c998efa309e47c69ff5b298c0aae59736
Orion <Oriondarley@gmail.com>
2020-02-24 19:56:30 -0600

    Add files via upload

---
+++ DataClass/VAE - generic.py
@@ -0,0 +1,12 @@
+import os
+
+import numpy as np
+import torch
+import torchvision.datasets as dset
+import torch.nn as nn
+import torchvision.transforms as transforms
+
+import pyro
+import pyro.distributions as dist
+from pyro.infer import SVI, Trace_ELBO
+from pyro.optim import Adam
