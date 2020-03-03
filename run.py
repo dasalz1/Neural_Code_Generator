290133a2077258b98b0d1ea33a396579ca71b1e0
Daniel Salz <dasalz@stanford.edu>
2020-03-02 18:44:39 -0800

    get_data

--- run.py
+++ run.py
@@ -38,17 +38,19 @@ def main(args):
 
 	repo_files = list(filter(lambda x: True if x.endswith('.csv') else False, next(os.walk(args.filepath))[2]))
 
-	data_loader = DataLoader(ConcatDataset([PairDataset(args.filepath +'/'+dataset) for dataset in repo_files[num_validation_repos:]]),
+	data_loader = DataLoader(ConcatDataset([PairDataset(args.filepath +'/'+dataset) for dataset in repo_files[:5]]),#num_validation_repos:]]),
 							batch_size=args.batch_size,
 							shuffle=True,
-							collate_fn=batch_collate_fn)
+							collate_fn=batch_collate_fn,
+							num_workers=16)
 
 	# data_loader = DataLoader(PairDataset(args.filepath+'/'+repo_files[30]), batch_size=args.batch_size, shuffle=True, collate_fn=batch_collate_fn)
 
-	validation_loader = DataLoader(ConcatDataset([PairDataset(args.filepath +'/'+dataset) for dataset in repo_files[:num_validation_repos]]),
+	validation_loader = DataLoader(ConcatDataset([PairDataset(args.filepath +'/'+dataset) for dataset in repo_files[:4]]),#:num_validation_repos]]),
 							batch_size=args.batch_size,
 							shuffle=True,
-							collate_fn=batch_collate_fn)
+							collate_fn=batch_collate_fn,
+							num_workers=16)
 
 	num_iterations = len(data_loader)
 
