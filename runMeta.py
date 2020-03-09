3f1eae327cd1a502dcc2e0da62e3323de959b6ee
Daniel Salz <dasalz@stanford.edu>
2020-03-09 02:34:04 -0700

    aa

--- runMeta.py
+++ runMeta.py
@@ -48,8 +48,8 @@ def main(args):
 		temp = MetaRepo(args.filepath+'/'+dataset, False, k_shot=args.k_shot, query_batch_size=args.query_batch_size)
 		if len(temp) > 0: data_loaders.append(DataLoader(temp, shuffle=True, batch_size=1))
 	for dataset in repo_files[:num_validation_repos]:
-		temp = DataLoader(MetaRepo(args.filepath+'/'+dataset, False, k_shot=args.k_shot, query_batch_size=args.query_batch_size), shuffle=True, batch_size=1)
-		if len(temp) > 0: validation_loaders.append(temp)
+		temp = MetaRepo(args.filepath+'/'+dataset, False, k_shot=args.k_shot, query_batch_size=args.query_batch_size)
+		if len(temp) > 0: validation_loaders.append(DataLoader(temp, shuffle=True, batch_size=1))
 	# data_loaders = [iter(DataLoader(MetaRepo(args.filepath+'/'+dataset, False, k_shot=args.k_shot, query_batch_size=args.query_batch_size), shuffle=True, batch_size=1)) 
 	# data_loaders = [iter(DataLoader(MetaRepo(args.filepath+'/'+dataset, False, k_shot=args.k_shot, query_batch_size=args.query_batch_size), shuffle=True, batch_size=1)) for dataset in repo_files[num_validation_repos:102]]
 	# validation_data_loaders = [iter(DataLoader(MetaRepo(args.filepath+'/'+dataset, False, k_shot=args.k_shot), shuffle=True, batch_size=1)) for dataset in repo_files[98:num_validation_repos]]
