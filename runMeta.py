7c0ec7ffe42dd58bb0b531b093c4fd8a290716bf
Daniel Salz <dasalz@stanford.edu>
2020-03-11 19:28:58 -0700

    more loaders

--- runMeta.py
+++ runMeta.py
@@ -59,7 +59,7 @@ def main(args):
 									n_retrieved=args.n_retrieved, k_shot=args.k_shot, 
 									query_batch_size=args.query_batch_size)
 
-		if len(temp) > 3: 
+		if len(temp) > 10: 
 			for i in range(100):
 				data_loaders.append(iter(DataLoader(temp, shuffle=True, batch_size=1)))
 
