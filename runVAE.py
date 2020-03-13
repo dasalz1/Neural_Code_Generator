8d38a3fcd14e8e31846fc872e21df4295dbdfa32
Daniel Salz <dasalz@stanford.edu>
2020-03-13 01:35:55 -0700

    a

--- runVAE.py
+++ runVAE.py
@@ -96,15 +96,12 @@ def main(args):
 	model.to(device)
 	checkpoint = torch.load("checkpoint-vae-120000.pth")
     
-    model.load_state_dict(checkpoint['model'])
+	model.load_state_dict(checkpoint['model'])
 
 	trainer = VAETrainer(device)
 
 	# trainer.train(model, data_loader, validation_loader, tb=tb, epochs=args.epochs)
-
-	
-
-	all_repos = [(DataLoader(PairDataset(args.filepath +'/'+dataset), batch_size=args.batch_size, collate_fn=batch_collate_fn, num_workers=64), args.filepath, dataset) for dataset in repo_files[num_validation_repos:]]
+	all_repos = [(DataLoader(PairDataset(args.filepath +'/'+dataset), batch_size=args.batch_size, collate_fn=batch_collate_fn, num_workers=64), args.filepath, dataset) for dataset in repo_files[num_validation_repos:100]]
 
 	trainer.create_files(model, all_repos)
 
